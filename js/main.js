842ce0115fc517edc15be2430712c103a96a73ff
BREWYM <basov0304@yandex.ru>
2023-03-19 01:22:00 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-export function randomIntInRange(min, max){
+export const randomIntInRange = (min, max) =>{
   const a = min(min, max);
   const b = max(min, max);
   return (Math.random()* (b - a) + a)
 
-}
+};
 
-export function stringLengthCheck(string, length){
+export const stringLengthCheck = (string, length) =>{
   return(string.length <= length)
-}
+};
