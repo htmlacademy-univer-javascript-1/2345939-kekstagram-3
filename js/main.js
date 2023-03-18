39b0ca5eb3e5ade1dcb423e596892b21b826bff3
BREWYM <basov0304@yandex.ru>
2023-03-19 01:25:28 +0300

    semicolon-_-

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 export const randomIntInRange = (min, max) =>{
   const a = min(min, max);
   const b = max(min, max);
-  return (Math.random()* (b - a) + a)
-
+  return (Math.random()* (b - a) + a);
 };
 
-export const stringLengthCheck = (string, length) =>{
-  return(string.length <= length)
-};
+export const stringLengthCheck = (string, length) =>
+  string.length <= length;
+
