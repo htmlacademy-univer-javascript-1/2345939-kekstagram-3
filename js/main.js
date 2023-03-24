872e68960a378470d14983783e96e3add78005c2
BREWYM <basov0304@yandex.ru>
2023-03-24 23:32:49 +0300

    flooring of result

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 export const randomIntInRange = (min, max) =>{
   const a = min(min, max);
   const b = max(min, max);
-  return (Math.random()* (b - a) + a);
+  return ((Math.random()* (b - a) + a).floor);
 };
 
 export const stringLengthCheck = (string, length) =>
