e1dfa9621a849cd0aa25236bd85fed0a780f91fb
BREWYM <basov0304@yandex.ru>
2023-05-04 18:50:18 +0300

    module 8.2

---
+++ js/form.js
@@ -0,0 +1,42 @@
+import { isEscKey } from './util.js';
+
+const downloadButton = document.querySelector('#upload-file');
+const cancelButton = document.querySelector('#upload-cancel');
+const hashtagInput = document.querySelector('.text__hashtags');
+const commentInput = document.querySelector('.text__description');
+
+function cleanForm() {
+  downloadButton.value = '';
+  hashtagInput.value = '';
+  commentInput.value = '';
+}
+
+const escapeOnForm = (evt) => {
+  if (isEscKey(evt)) {
+    evt.preventDefault();
+    closeDownloadPhotoWindow();
+  }
+};
+
+function openDownloadPhotoWindow() {
+  document.querySelector('.img-upload__overlay').classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', escapeOnForm);
+}
+
+function closeDownloadPhotoWindow() {
+  document.querySelector('.img-upload__overlay').classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', escapeOnForm);
+  cleanForm();
+}
+
+downloadButton.addEventListener('change', () => {
+  openDownloadPhotoWindow();
+});
+
+
+cancelButton.addEventListener('click', () => {
+  closeDownloadPhotoWindow();
+});
